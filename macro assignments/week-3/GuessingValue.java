81c396daa42c0c8c1d6917f90bb1ceb05d68adbd
ranjitha b <ranjithabv2001@gmail.com>
2023-02-27 08:35:11 +0530

    done

---
+++ macro assignments/week-3/GuessingValue.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class GuessingValue{
+  public static void main(String args[]){
+ Scanner sc=new Scanner(System.in);
+    Random random = new Random(); 
+    int randomnumber=random.nextInt(100);
+    System.out.println(randomnumber);
+    int Guess=0;
+    do{
+      System.out.println("Guess Number:");
+      Guess=sc.nextInt();
+      if(randomnumber==Guess)
+        System.out.println(" Correct Guess!!Congrats");
+      else
+        System.out.println(" Incorrect Guess!!Try Again");
+    }while(randomnumber!=Guess);
+   }
+}
+    
+
+    
+   
